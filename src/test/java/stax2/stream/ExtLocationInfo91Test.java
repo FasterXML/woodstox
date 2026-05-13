494d152289f2af67e9100ae5b16f8caff6ef3edf
Tatu Saloranta <tatu.saloranta@iki.fi>
2026-05-13 16:52:25 -0700

    Moar test coverage

--- src/test/java/stax2/stream/ExtLocationInfo91Test.java
+++ src/test/java/stax2/stream/ExtLocationInfo91Test.java
@@ -63,11 +63,8 @@ public class ExtLocationInfo91Test
         assertEquals("include", sr.getLocalName());
         assertOffset(sr, 0, 9, INCL_URI);
 
-
-
         assertTokenType(END_ELEMENT, sr.next());
         assertOffset(sr, 9, 19, INCL_URI);
-
 
         assertTokenType(END_ELEMENT, sr.next());
         assertOffset(sr, 89, 96, URI);
@@ -220,10 +217,164 @@ public class ExtLocationInfo91Test
         sr.close();
     }
 
+    // [woodstox-core#91] Verify row/column tracking on entry into a
+    // multi-line external entity, and that row counting in the parent
+    // is unaffected when we return from it.
+    public void testMultiLineExtEntity()
+        throws XMLStreamException
+    {
+        final String mainXml =
+            "<!DOCTYPE main ["
+            + "<!ENTITY ext SYSTEM 'ext.xml'>"
+            + "]>"
+            + "<main>&ext;</main>";
+        final String extXml = "<r>\n  <a/>\n</r>";
+        // ext.xml byte layout:
+        //   <r>     : 0..2  (end 3)   row 1
+        //   \n      : 3                row 1 -> 2 after
+        //     <a/>  : 6..9  (end 10)  row 2  (col 3)
+        //   \n      : 10               row 2 -> 3 after
+        //   </r>    : 11..14 (end 15) row 3
+
+        XMLStreamReader2 sr = getReader(mainXml, URI, resolverFor("ext.xml", extXml));
+
+        assertTokenType(DTD, sr.next());
+        assertTokenType(START_ELEMENT, sr.next());
+        assertEquals("main", sr.getLocalName());
+
+        assertTokenType(START_ELEMENT, sr.next());
+        assertEquals("r", sr.getLocalName());
+        assertLocation(sr, "ext.xml", 0, 1, 1);
+
+        // CHARACTERS for "\n  " (with non-coalescing this is one event)
+        assertTokenType(CHARACTERS, sr.next());
+        assertLocation(sr, "ext.xml", 3, 1, 4);
+
+        assertTokenType(START_ELEMENT, sr.next());
+        assertEquals("a", sr.getLocalName());
+        assertLocation(sr, "ext.xml", 6, 2, 3);
+
+        assertTokenType(END_ELEMENT, sr.next());
+        assertEquals("a", sr.getLocalName());
+
+        // CHARACTERS for "\n"
+        assertTokenType(CHARACTERS, sr.next());
+        assertLocation(sr, "ext.xml", 10, 2, 7);
+
+        assertTokenType(END_ELEMENT, sr.next());
+        assertEquals("r", sr.getLocalName());
+        assertLocation(sr, "ext.xml", 11, 3, 1);
+
+        assertTokenType(END_ELEMENT, sr.next());
+        assertEquals("main", sr.getLocalName());
+        assertSystemId(sr, URI);
+
+        sr.close();
+    }
+
+    // [woodstox-core#91] Exit case where the first char in the parent after
+    // the external entity is a newline. Verifies that re-anchored token-start
+    // (row, col) is correct.
+    public void testNewlineAfterExtEntityInParent()
+        throws XMLStreamException
+    {
+        final String mainXml =
+            "<!DOCTYPE main ["
+            + "<!ENTITY ext SYSTEM 'ext.xml'>"
+            + "]>"
+            + "<main>&ext;\n<x/></main>";
+        final String extXml = "<a/>";
+        // main.xml byte layout:
+        //   <!DOCTYPE main [               16
+        //   <!ENTITY ext SYSTEM 'ext.xml'> 30
+        //   ]>                              2  -> ends at offset 48
+        //   <main>                          6  -> 48..53 (end 54)
+        //   &ext;                           5  -> 54..58 (end 59)
+        //   \n                              1  -> 59     (end 60)
+        //   <x/>                            4  -> 60..63 (end 64)
+        //   </main>                         7  -> 64..70 (end 71)
+
+        XMLStreamReader2 sr = getReader(mainXml, URI, resolverFor("ext.xml", extXml));
+
+        assertTokenType(DTD, sr.next());
+        assertTokenType(START_ELEMENT, sr.next());
+        assertEquals("main", sr.getLocalName());
+
+        // First event from the entity:
+        assertTokenType(START_ELEMENT, sr.next());
+        assertEquals("a", sr.getLocalName());
+        assertLocation(sr, "ext.xml", 0, 1, 1);
+
+        assertTokenType(END_ELEMENT, sr.next());
+        assertEquals("a", sr.getLocalName());
+
+        // The first event back in the parent should be whitespace CHARACTERS
+        // for the '\n', located at the '\n' position (row 1 in main.xml).
+        assertTokenType(CHARACTERS, sr.next());
+        assertLocation(sr, URI, 59, 1, 60);
+
+        assertTokenType(START_ELEMENT, sr.next());
+        assertEquals("x", sr.getLocalName());
+        // '<' of <x/> is on row 2, col 1 in main.xml
+        assertLocation(sr, URI, 60, 2, 1);
+
+        assertTokenType(END_ELEMENT, sr.next());
+        assertEquals("x", sr.getLocalName());
+
+        assertTokenType(END_ELEMENT, sr.next());
+        assertEquals("main", sr.getLocalName());
+        // '<' of </main> on row 2
+        assertLocation(sr, URI, 64, 2, 5);
+
+        sr.close();
+    }
+
+    // [woodstox-core#91] With coalescing enabled, one CHARACTERS event spans
+    // before-entity / entity content / after-entity text. The event
+    // straddles a boundary; verify start is anchored at the first parent
+    // char and end at the last parent char, both with the parent system id.
+    public void testCoalescingAcrossExtEntity()
+        throws XMLStreamException
+    {
+        final String mainXml =
+            "<!DOCTYPE main ["
+            + "<!ENTITY ext SYSTEM 'ext.xml'>"
+            + "]>"
+            + "<main>before&ext;after</main>";
+        final String extXml = "MIDDLE";
+        // main.xml byte layout:
+        //   <!DOCTYPE main [...]>             48
+        //   <main>                            48..53 (end 54)
+        //   before                            54..59 (end 60)
+        //   &ext;                             60..64 (end 65)
+        //   after                             65..69 (end 70)
+        //   </main>                           70..76 (end 77)
+
+        XMLStreamReader2 sr = getReader(mainXml, URI, resolverFor("ext.xml", extXml), true);
+
+        assertTokenType(DTD, sr.next());
+        assertTokenType(START_ELEMENT, sr.next());
+        assertEquals("main", sr.getLocalName());
+
+        assertTokenType(CHARACTERS, sr.next());
+        assertEquals("beforeMIDDLEafter", sr.getText());
+        // Start anchored at 'b' of "before" in main.xml; end after 'r' of "after".
+        LocationInfo li = sr.getLocationInfo();
+        assertEquals("Wrong start systemID", URI, li.getStartLocation().getSystemId());
+        assertEquals("Wrong end systemID",   URI, li.getEndLocation().getSystemId());
+        assertEquals("Wrong start offset",   54, li.getStartLocation().getCharacterOffset());
+        assertEquals("Wrong end offset",     70, li.getEndLocation().getCharacterOffset());
+
+        assertTokenType(END_ELEMENT, sr.next());
+        assertEquals("main", sr.getLocalName());
+
+        sr.close();
+    }
+
     /*
-    ////////////////////////////////////////
+    ///////////////////////////////////////////////////////////
     // Private methods
-    ////////////////////////////////////////
+    ///////////////////////////////////////////////////////////
      */
 
     private XMLResolver resolverFor(final String wantSystemId, final String contents)
@@ -247,6 +398,19 @@ public class ExtLocationInfo91Test
                 expected, li.getEndLocation().getSystemId());
     }
 
+    private void assertLocation(XMLStreamReader2 sr, String systemId,
+                                int startOffset, int startRow, int startCol)
+        throws XMLStreamException
+    {
+        LocationInfo li = sr.getLocationInfo();
+        Location loc = li.getStartLocation();
+        String evt = tokenTypeDesc(sr.getEventType());
+        assertEquals("Wrong starting systemID for " + evt, systemId, loc.getSystemId());
+        assertEquals("Wrong starting offset for " + evt, startOffset, loc.getCharacterOffset());
+        assertEquals("Wrong starting row for " + evt, startRow, loc.getLineNumber());
+        assertEquals("Wrong starting col for " + evt, startCol, loc.getColumnNumber());
+    }
+
     private void assertOffset(XMLStreamReader2 sr, int startOffset, int endOffset, String systemId)
         throws XMLStreamException
     {
@@ -267,8 +431,14 @@ public class ExtLocationInfo91Test
         assertEquals("Incorrect ending offset for event "+tokenTypeDesc(sr.getEventType()), endOffset, endLoc.getCharacterOffset());
     }
 
-
     private XMLStreamReader2 getReader(String contents, String systemId, XMLResolver xmlResolver)
+        throws XMLStreamException
+    {
+        return getReader(contents, systemId, xmlResolver, false);
+    }
+
+    private XMLStreamReader2 getReader(String contents, String systemId, XMLResolver xmlResolver,
+                                       boolean coalescing)
         throws XMLStreamException
     {
         XMLInputFactory f = getInputFactory();
@@ -277,7 +447,7 @@ public class ExtLocationInfo91Test
             f.setXMLResolver(xmlResolver);
         }
 
-        setCoalescing(f, false); // shouldn't really matter
+        setCoalescing(f, coalescing);
         setNamespaceAware(f, true);
         setSupportExternalEntities(f, true);
         setReplaceEntities(f, true);
