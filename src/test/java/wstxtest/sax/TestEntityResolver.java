b79aa1957091787e834aa0738077da43050620dc
Tatu Saloranta <tatu.saloranta@iki.fi>
2026-05-12 20:26:19 -0700

    Fix #226: `WstxSAXParser` does not inherit the factory configuration

--- src/test/java/wstxtest/sax/TestEntityResolver.java
+++ src/test/java/wstxtest/sax/TestEntityResolver.java
@@ -4,12 +4,15 @@ import java.io.*;
 
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
+import javax.xml.stream.XMLResolver;
+import javax.xml.stream.XMLStreamException;
 
 import org.xml.sax.*;
 import org.xml.sax.helpers.DefaultHandler;
 
 import com.ctc.wstx.exc.WstxException;
 import com.ctc.wstx.sax.WstxSAXParserFactory;
+import com.ctc.wstx.stax.WstxInputFactory;
 
 import wstxtest.BaseWstxTest;
 
@@ -62,6 +65,43 @@ public class TestEntityResolver
         } catch (SAXException e) {
             fail("Should not have failed with entity resolver, got ("+e.getClass()+"): "+e.getMessage());
         }
+    }
+
+    /**
+     * Test for [woodstox-core#226]: the {@link XMLResolver} configured on the
+     * underlying {@link WstxInputFactory} should be used by the SAX parser
+     * when no SAX EntityResolver is explicitly set on the parser itself.
+     */
+    public void testFactoryXMLResolverIsInherited()
+        throws Exception
+    {
+        final String XML =
+            "<!DOCTYPE root PUBLIC '//some//public//id' 'no-such-thing.dtd'>\n"
+            +"<root />"
+            ;
+
+        WstxInputFactory inputFactory = new WstxInputFactory();
+        final boolean[] resolverCalled = new boolean[] { false };
+        inputFactory.setXMLResolver(new XMLResolver() {
+            @Override
+            public Object resolveEntity(String publicID, String systemID, String baseURI, String namespace)
+                throws XMLStreamException {
+                resolverCalled[0] = true;
+                // Return an empty stream, equivalent to an empty DTD subset
+                return new ByteArrayInputStream(new byte[0]);
+            }
+        });
+        WstxSAXParserFactory spf = new WstxSAXParserFactory(inputFactory);
+        SAXParser sp = spf.newSAXParser();
+
+        DefaultHandler h = new DefaultHandler();
+        try {
+            sp.parse(new InputSource(new StringReader(XML)), h);
+        } catch (SAXException e) {
+            fail("Should not have failed when XMLResolver is set on factory, got ("
+                    + e.getClass() + "): " + e.getMessage());
+        }
+        assertTrue("Expected XMLResolver configured on factory to be invoked", resolverCalled[0]);
     }
 
     /*
