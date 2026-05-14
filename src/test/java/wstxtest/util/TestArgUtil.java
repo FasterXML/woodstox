bbd30fcd5f69669539853cc10efddb004a0fe0b7
Tatu Saloranta <tatu.saloranta@iki.fi>
2026-05-13 21:50:48 -0700

    Add more unit tests

--- src/test/java/wstxtest/util/TestArgUtil.java
+++ src/test/java/wstxtest/util/TestArgUtil.java
@@ -34,6 +34,12 @@ public class TestArgUtil
         } catch (IllegalArgumentException iae) { }
     }
 
+    public void testBooleanFromBoolean()
+    {
+        assertTrue(ArgUtil.convertToBoolean("test", Boolean.TRUE));
+        assertFalse(ArgUtil.convertToBoolean("test", Boolean.FALSE));
+    }
+
     public void testInt()
     {
         assertEquals(14, ArgUtil.convertToInt("test", "14", 0));
@@ -41,6 +47,10 @@ public class TestArgUtil
         assertEquals(14, ArgUtil.convertToInt("test", 14L, 0));
         assertEquals(14, ArgUtil.convertToInt("test", (short) 14, 0));
         assertEquals(14, ArgUtil.convertToInt("test", (byte) 14, 0));
+
+        // null is converted to 0 (and 0 must be >= minValue)
+        assertEquals(0, ArgUtil.convertToInt("test", null, 0));
+        assertEquals(0, ArgUtil.convertToInt("test", null, -10));
 
         // and then errors:
         try {
@@ -52,5 +62,73 @@ public class TestArgUtil
             /*int x =*/ ArgUtil.convertToInt("test", "foobar", 0);
             fail("Expected an IllegalArgumentException");
         } catch (IllegalArgumentException iae) { }
+    }
+
+    public void testIntBelowMinimum()
+    {
+        // Value below minValue should throw IllegalArgumentException
+        try {
+            ArgUtil.convertToInt("test", 3, 10);
+            fail("Expected an IllegalArgumentException");
+        } catch (IllegalArgumentException iae) {
+            verifyException(iae, "minimum is 10");
+        }
+
+        // Same for a String that parses to below-minimum
+        try {
+            ArgUtil.convertToInt("test", "3", 10);
+            fail("Expected an IllegalArgumentException");
+        } catch (IllegalArgumentException iae) {
+            verifyException(iae, "minimum is 10");
+        }
+    }
+
+    public void testLong()
+    {
+        assertEquals(99L, ArgUtil.convertToLong("test", "99", 0L));
+        assertEquals(99L, ArgUtil.convertToLong("test", 99L, 0L));
+        assertEquals(99L, ArgUtil.convertToLong("test", 99, 0L));
+        assertEquals(99L, ArgUtil.convertToLong("test", (short) 99, 0L));
+        assertEquals(99L, ArgUtil.convertToLong("test", (byte) 99, 0L));
+
+        // null becomes 0
+        assertEquals(0L, ArgUtil.convertToLong("test", null, 0L));
+        assertEquals(0L, ArgUtil.convertToLong("test", null, -10L));
+
+        // bad String:
+        try {
+            ArgUtil.convertToLong("test", "not-a-number", 0L);
+            fail("Expected an IllegalArgumentException");
+        } catch (IllegalArgumentException iae) {
+            verifyException(iae, "Long");
+        }
+
+        // unsupported type:
+        try {
+            ArgUtil.convertToLong("test", new HashMap<>(), 0L);
+            fail("Expected an IllegalArgumentException");
+        } catch (IllegalArgumentException iae) {
+            verifyException(iae, "expected Long");
+        }
+    }
+
+    public void testLongBelowMinimum()
+    {
+        try {
+            ArgUtil.convertToLong("test", 3L, 10L);
+            fail("Expected an IllegalArgumentException");
+        } catch (IllegalArgumentException iae) {
+            verifyException(iae, "minimum is 10");
+        }
+    }
+
+    private void verifyException(Throwable e, String substr)
+    {
+        String msg = e.getMessage();
+        assertNotNull("Exception message should not be null", msg);
+        if (msg.indexOf(substr) < 0) {
+            fail("Expected exception message to contain '" + substr
+                    + "', got: " + msg);
+        }
     }
 }
