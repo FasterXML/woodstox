6d378c0f84be379a576b87e586067c3829f50032
Tatu Saloranta <tatu.saloranta@iki.fi>
2026-05-14 15:14:53 -0700

    ...

--- src/test/java/wstxtest/evt/TestEventFactoryLocation.java
+++ src/test/java/wstxtest/evt/TestEventFactoryLocation.java
@@ -1,15 +1,12 @@
 package wstxtest.evt;
 
-import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.Objects;
 
 import javax.xml.stream.Location;
 import javax.xml.stream.events.XMLEvent;
 
-import org.hamcrest.Description;
-import org.hamcrest.Matcher;
-import org.hamcrest.TypeSafeMatcher;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -29,7 +26,7 @@ public class TestEventFactoryLocation {
     public void testDefaultLocation() {
         XMLEvent event = eventFactory.createStartDocument();
 
-        assertThat("event.location", event.getLocation(), unknownLocation());
+        assertLocationProperties(event.getLocation(), "", "", -1, -1, -1);
     }
 
     @Test
@@ -39,7 +36,7 @@ public class TestEventFactoryLocation {
 
         XMLEvent event = eventFactory.createStartElement("foo", "bar", "baz");
 
-        assertThat("event.location", event.getLocation(), unknownLocation());
+        assertLocationProperties(event.getLocation(), "", "", -1, -1, -1);
     }
 
     @Test
@@ -51,35 +48,18 @@ public class TestEventFactoryLocation {
         volatileLocation.line = 4;
         volatileLocation.col = 5;
 
-        assertThat("event.location", event.getLocation(),
-                locationWithProperties(null, null, -1, 2, 3));
+        assertLocationProperties(event.getLocation(), null, null, -1, 2, 3);
     }
 
-    private static Matcher<Location> unknownLocation() {
-        // XXX: Not sure if the empty-string publicId/systemId are conformant
-        //return locationWithProperties(null, null, -1, -1, -1);
-        return locationWithProperties("", "", -1, -1, -1);
-    }
-
-    private static Matcher<Location> locationWithProperties(final String pubId,
-            final String sysId, final int charOffset, final int row, final int col) {
-        return new TypeSafeMatcher<Location>() {
-            @Override public void describeTo(Description description) {
-                description.appendText("Location(publicId: ").appendValue(pubId)
-                           .appendText(", systemId: ").appendValue(sysId)
-                           .appendText(", characterOffset: ").appendValue(charOffset)
-                           .appendText(", lineNumber: ").appendValue(row)
-                           .appendText(", columnNumber: ").appendValue(col);
-            }
-
-            @Override protected boolean matchesSafely(Location item) {
-                return Objects.equals(item.getPublicId(), pubId)
-                        && Objects.equals(item.getSystemId(), sysId)
-                        && Objects.equals(item.getCharacterOffset(), charOffset)
-                        && Objects.equals(item.getLineNumber(), row)
-                        && Objects.equals(item.getColumnNumber(), col);
-            }
-        };
+    private static void assertLocationProperties(Location loc, String pubId,
+            String sysId, int charOffset, int row, int col) {
+        assertEquals(pubId, loc.getPublicId(), "publicId");
+        assertEquals(sysId, loc.getSystemId(), "systemId");
+        assertEquals(charOffset, loc.getCharacterOffset(), "characterOffset");
+        assertEquals(row, loc.getLineNumber(), "lineNumber");
+        assertEquals(col, loc.getColumnNumber(), "columnNumber");
+        // sanity check that we used Objects.equals semantics
+        assert Objects.equals(loc.getLineNumber(), row);
     }
 
     static class VolatileLocation implements Location {
